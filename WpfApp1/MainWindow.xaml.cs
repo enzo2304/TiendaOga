38be767688a6b4c914ce6adb29487c35d4cb5b57
Enzo <enzo-2304@hotmail.com>
2026-08-26 23:38:54 -0300

    Se agrego validacion al ingresar los datos del usuario

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,13 +27,31 @@ namespace TiendaOga
 
         private void btnIngresar_Click(object sender, RoutedEventArgs e)
         {
-            Window1 ventanaDashboard = new Window1();
+            // 1. Capturamos lo que escribió el usuario (Trim borra espacios accidentales)
+            string usuario = txtUsuario.Text.Trim();
+            string password = txtPassword.Password.Trim(); // Ojo: PasswordBox usa .Password, no .Text
 
-            // Le decimos a Visual Studio que la muestre en la pantalla
-            ventanaDashboard.Show();
+            // 2. CONDICIÓN: Validar que no estén vacíos
+            if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Por favor, ingrese un usuario y una contraseña.", "Datos faltantes", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return; // El "return" frena el código acá, no sigue leyendo hacia abajo
+            }
 
-          
-            this.Close();
+            // 3. SIMULACIÓN DE BASE DE DATOS (Temporal)
+            // Cuando conectemos la BD, reemplazaremos este "if" por un "SELECT * FROM Usuario..."
+            if (usuario == "admin" && password == "1234")
+            {
+                // Si ingresa bien, abrimos el Dashboard
+                Window1 ventanaDashboard = new Window1();
+                ventanaDashboard.Show();
+                this.Close(); // Cierra el login
+            }
+            else
+            {
+                // Si ingresa mal, mostramos error
+                MessageBox.Show("Usuario o contraseña incorrectos.", "Error de acceso", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
