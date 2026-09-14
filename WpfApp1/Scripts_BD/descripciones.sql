fff8069fc6388de5b5e8a496928a066626106e92
enzo2304 <enzo-2304@hotmail.com>
2026-09-14 12:02:00 -0300

    descripciones sql

---
+++ WpfApp1/Scripts_BD/descripciones.sql
@@ -0,0 +1,3 @@
+INSERT INTO dbo.Perfiles (nombre_perfil, descripcion, Activo) VALUES
+('Gerente', 'Perfil de gestión con permisos intermedios', 1),
+('Vendedor', 'Perfil de ventas con acceso limitado', 1);
